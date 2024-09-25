1b6204934e4ad2617649586937e41f3b22389397
shunGoes <shunnode@gmail.com>
2024-09-25 12:16:20 +0100

    updated cors

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const cors = require("cors")
 const nodemailer = require("nodemailer")
 
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({ origin: "*" }));
 app.use(express.json({limit: "25mb"}))
 app.use(express.urlencoded({limit: "25mb"}))
 
